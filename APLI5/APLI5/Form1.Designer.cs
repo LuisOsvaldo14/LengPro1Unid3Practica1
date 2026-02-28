cb174aecbe5e3e042a816031f7748b460452aea9
Luis Osvaldo Peralta del Villar <thenewbie.soy@gmail.com>
2026-02-28 17:28:31 -0400

    Ejercicio 5

---
+++ APLI5/APLI5/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace APLI5
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            maskedTextBox1 = new MaskedTextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(332, 153);
+            maskedTextBox1.Mask = "00000";
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(100, 23);
+            maskedTextBox1.TabIndex = 0;
+            maskedTextBox1.Tag = "";
+            maskedTextBox1.MaskInputRejected += maskedTextBox1_MaskInputRejected;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(303, 206);
+            button1.Name = "button1";
+            button1.Size = new Size(152, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Verificar si es primo o no";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(maskedTextBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MaskedTextBox maskedTextBox1;
+        private Button button1;
+    }
+}
---
+++ APLI5/APLI5/Form1.cs
@@ -0,0 +1,42 @@
+namespace APLI5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int Numero = int.Parse(maskedTextBox1.Text);
+            int Contador = 0;
+
+            for (int i = 0; i <= Numero; i++)
+            {
+                if (Numero % i == 0)
+                {
+                    Contador++;
+                }
+
+            }
+            if (Contador == 2)
+            {
+                MessageBox.Show("Este Numero es Primo");
+            }
+            else
+            {
+                MessageBox.Show("Este Numero no es Primo");
+            }
+            
+
+
+
+        }
+    }
+}
